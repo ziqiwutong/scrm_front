dd26ebfff75cbb5e254c0b746b5819e3a3a6f4a2
何甜甜 <1731098360@qq.com>
2021-10-14 12:08:05 +0800

    '提交文章详情的部分代码'

---
+++ src/components/component/HeaderNavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <span class="returnIcon"></span>
+    <span class="return">返回</span>
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderNavBar",
+  props:[
+    "title"
+  ]
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+  height: 44px;
+  position: fixed;
+  text-align: center;
+  width: 100vw;
+  top: 0px;
+  background-color: white;
+}
+
+.header span {
+  line-height: 44px;
+}
+
+.return {
+  position: absolute;
+  top: 0px;
+  left: 30px;
+}
+
+.returnIcon {
+  position: absolute;
+  top: 18px;
+  left: 15px;
+  border-top: 1px solid;
+  border-right: 1px solid;
+  width: 8px;
+  height: 8px;
+  border-color: #5f6368;
+  transform: rotate(-135deg);
+}
+</style>
