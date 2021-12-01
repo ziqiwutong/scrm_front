10e2ac866219dce6de5bd57fd026ffaaa34964a2
zujiasheng <zujiasheng@qq.com>
2021-12-01 15:13:06 +0800

    zjs

--- config/index.js
+++ config/index.js
@@ -11,29 +11,17 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-<<<<<<< HEAD
         target: 'https://www.fzk-tx.top/', // 要访问的接口域名
         // target: 'http://192.168.1.101:30001/', // 要访问的接口域名
         // target: 'http://127.0.0.1:3000/', // 要访问的接口域名
         // target: 'http://192.168.1.108:30000/', // 要访问的接口域名
-=======
-        // target: 'http://101.132.148.145/', // 要访问的接口域名
-        // target: 'https://www.fzk-tx.top/', // 要访问的接口域名
-        // target: 'https://www.fastmock.site/mock/e89826b10151d3ddafd81e87b0cf7110/api', // 要访问的接口域名
 
-        target: 'https://www.fzk-tx.top/',
-        // target: 'https://www.fastmock.site/mock/e89826b10151d3ddafd81e87b0cf7110/api/', // 要访问的接口域名
-        // target: 'http://192.168.1.108:30002/',  //后端接口
-        // target: 'https://www.fastmock.site/mock/e89826b10151d3ddafd81e87b0cf7110/api/', // 要访问的接口域名
-
->>>>>>> zjs
         ws: true, // 是否启用websockets
         changeOrigin: true, // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
           '^/api': '' // 这里理解成用'/api'代替target里面的地址,比如我要调用'http://40.00.100.100:3002/user/add'，直接写'/api/user/add'即可
         }
       },
-<<<<<<< HEAD
       '/sweet': {
         target: 'https://www.fastmock.site/mock/e89826b10151d3ddafd81e87b0cf7110/api/', // 要访问的接口域名
         ws: true, // 是否启用websockets
@@ -66,13 +54,13 @@ module.exports = {
           '^/wxResource': ''
         }
       },
-      '/wx':{
+      '/wx': {
         target: 'http://127.0.0.1:3001/', // 要访问的接口域名
         ws: true, // 是否启用websockets
         changeOrigin: true, // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
           '^/wx': ''
-=======
+        },
       '/fzk': {
         // target: 'http://101.132.148.145/', // 要访问的接口域名
         target: 'https://www.fzk-tx.top/', // 要访问的接口域名
@@ -83,7 +71,6 @@ module.exports = {
         changeOrigin: true, // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
           '^/fzk': '' // 这里理解成用'/api'代替target里面的地址,比如我要调用'http://40.00.100.100:3002/user/add'，直接写'/api/user/add'即可
->>>>>>> zjs
         }
       }
     },
